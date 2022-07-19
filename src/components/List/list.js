306b45c01e321f7fe02fc7e6d99687c0c371a662
yuladevin <yuladevin@gmail.com>
2022-07-19 12:25:47 +0300

    add list component

---
+++ src/components/List/list.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+const List = () => {
+    return (
+        <>
+            <div>
+                <ul>
+                    <li>item</li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default List;
