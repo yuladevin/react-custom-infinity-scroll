10dd51f7e0d9e91b674602f104a0a4d10e791700
yuladevin <yuladevin@gmail.com>
2022-07-20 07:55:56 +0300

    optimization: add whyDidYouRender for dev

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// if (process.env.NODE_ENV === 'development') {
+//     const whyDidYouRender = require('@welldone-software/why-did-you-render');
+//     whyDidYouRender(React, {
+//         trackAllPureComponents: true,
+//     });
+// }
 ReactDOM.render(
   <React.StrictMode>
     <App />
