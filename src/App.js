0ca67dcc44ad9f8a01a852938e80a8c6b0d6416e
yuladevin <yuladevin@gmail.com>
2022-07-21 07:53:22 +0300

    create component scrollDomUpdate

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
-import React, { useState, useCallback, memo, lazy, Suspense } from "react"
-import throttle from "lodash/throttle"
-import {nanoid} from "nanoid"
-
-const List = lazy(()=>import('./components/List/list'))
-const LazyLoad = lazy(()=>import('./components/LazyLoad/lazyLoad'))
-const ScrollContainer = lazy(()=>import('./components/ScrollConteiner/scrollContainer'))
+import React from "react"
+import {ScrollDomUpdate} from "./components/ScrollDomUpdate/scrollDomUpdate";
+import MY_ENDLESS_LIST from "./mock/Constants";
 
 function App() {
-	const [scrollContainerRect, setScrollContainerRect] = useState({})
-	const [scrollTop, setScrollTop] = useState(0)
-	const [items, setItems] = useState(
-		Array(50).fill("").map(item=>nanoid(5))
-	)
-
-	const onScroll = useCallback(
-		throttle(({ scrollTop, scrollContainerRect }) => {
-			setScrollContainerRect(scrollContainerRect)
-			setScrollTop(scrollTop)
-		}, 10),
-		[setScrollContainerRect, setScrollTop]
-	)
-
-	const onMount = useCallback(
-		ref => {
-			setScrollContainerRect(ref.getBoundingClientRect())
-		},
-		[setScrollContainerRect]
-	)
-
-	const appendItems = useCallback(() => {
-		setItems([
-			...items,
-			...Array(10).fill("").map(item=>nanoid(5))
-		])
-	}, [items, setItems])
-
 	return (
 		<div className="App">
-			<Suspense fallback="Loading...">
-				<ScrollContainer onScroll={onScroll} onMount={onMount}>
-					<Suspense fallback="Loading...">
-						<LazyLoad
-							scrollTop={scrollTop}
-							scrollContainerRect={scrollContainerRect}
-							onIntersection={appendItems}
-						>
-							<Suspense fallback="Loading...">
-								<List itemsArray={items} />
-							</Suspense>
-						</LazyLoad>
-					</Suspense>
-				</ScrollContainer>
-			</Suspense>
+			<ScrollDomUpdate list={MY_ENDLESS_LIST} height={195}/>
 		</div>
 	)
 }
-export default memo(App)
+export default App
